ad30f2f905e9495694bdbf84017d34bc234f2ded
krishangguptafibonacciresearch <krishanggupta@fibonacciresearch.in>
2025-01-03 20:26:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("Live Plots with Persistent Links")
 
 # Persistent OneDrive public links
 plot_urls = [
-    "https://raw.githubusercontent.com/krishangguptafibonacciresearch/distro_project/main/ZN_1m_Volatility_Distribution_2024-09-30_2024-12-26_High_Low_.png"]
+    "https://raw.githubusercontent.com/krishangguptafibonacciresearch/distro_project/main/ZN_1m_Volatility_Distribution_2024-09-30_2024-12-26_High_Low_.png"
 ]
 
 st.header("Generated Plots")
